d4a5b8e6d86a3b41c1646fb04f8c2a2552e4e137
Sandro Ropelato <sandro.ropelato@gmail.com>
2011-09-22 21:29:24 +0200

    changed WebGL to Web GL.

--- home.php
+++ home.php
@@ -3,7 +3,7 @@
 	Meetpoint 3D is a test project to evaluate WebGL, a new web standard for hardware accelerated 3D graphics within web browsers.<br/>
 	In a first version, the user will be able to control an avatar across a virtual 3-dimensional world and chat with other real players. Additional features may be added to the software later.
 </p>
-<div class="title2">WebGL</div>
+<div class="title2">Web GL</div>
 <p>
 	WebGL is a Web-based Graphics Library. It extends the capability of the JavaScript programming language to allow it to generate interactive 3D graphics within any compatible web browser.<br/>
 	WebGL is a context of the canvas HTML element that provides a 3D computer graphics API without the use of plug-ins. The specification was released as version 1.0 on March 3, 2011. WebGL is managed by the non-profit Khronos Group.
